9c63444e0d358b14055f8666ab9cffc48f4927ea
noviannurrohman <noviannurrohman99@gmail.com>
2021-04-09 23:34:59 +0700

    laravelapp-crud 3

---
+++ week 12/laravel-crud/resources/views/master.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+	<title>@yield('title')</title>
+</head>
+<body>
+	<div class="container">
+		<div class="row mt-3">
+			<div class="col-md-6">
+				<div class="card">
+					<div class="card-header text-center">
+						<!-- bagian judul halaman -->
+						<h2> @yield('judul_halaman')</h2>
+					</div>
+					<div class="card-body">
+						<!-- bagian konten blog -->
+						@yield('konten')
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
